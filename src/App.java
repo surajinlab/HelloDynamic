1a6a4b507a514a26db0c0f88ea284fbc8e768483
surajinlab <suraj.in.lab@gmail.com>
2026-01-28 21:37:11 +0530

    second commit!

--- src/App.java
+++ src/App.java
@@ -5,6 +5,10 @@ public class App {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Your Name : ");
         String  name = sc.nextLine();
-        System.out.println("Hello " + name);
+        System.out.println("Hello  " + name);
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
     }
 }
